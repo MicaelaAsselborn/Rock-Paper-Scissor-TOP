81e453c14fcc7cb61327333f6296a3126b62e39f
Micaela Asselborn <asselborn96@gmail.com>
2025-02-20 15:29:15 -0300

    Computer can choose randomly one of the 3 options: rock, paper or scissors

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const getComputerChoice = Math.floor(Math.random() * 3);
+const options = ["ROCK", "PAPER", "SCISSORS"];
+const computerChoice = options[getComputerChoice];
+console.log(computerChoice)
